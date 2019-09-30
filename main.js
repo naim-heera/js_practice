1ee243dab045e43631cd11e66a715de6f19162b4
Naim Heera <abunaimheera@gmail.com>
2019-09-30 17:41:44 +0300

    Js2

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const para = document.querySelector('p');
+function createPara() {
+  let para = document.createElement('p');
+  para.textContent = 'You clicked the button';
+  document.body.appendChild(para);
+}
 
-para.addEventListener('click', updateName);
 
-function updateName() {
 
-  let name = prompt('Enter a new name');
-  para.textContent = 'player 1: ' + name;
+const buttons = document.querySelectorAll('button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', createPara);
 }
